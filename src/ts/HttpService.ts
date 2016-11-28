380ea194d925af223047809429192cf533e52b5b
Vasili Raby <vasiliraby@gmail.com>
2016-11-28 15:06:53 +0300

    update http serice

--- src/ts/HttpService.ts
+++ src/ts/HttpService.ts
@@ -1,7 +1,5 @@
 export default class HttpService {
     makeRequest = (url: string) => {
-        return window.fetch(url).then((response) => {
-            return response.json()
-        });
+        return window.fetch(url);
     };
 };
--- src/ts/app.ts
+++ src/ts/app.ts
@@ -13,9 +13,17 @@ locationService.getLocation().then((pos: Position) => {
 let httpService: HttpService = new HttpService();
 
 httpService.makeRequest('http://api.openweathermap.org/data/2.5/weather?lat=53.904539799999995&lon=27.5615244&APPID=103b41f82bea70d2198ab91ea029dcde').then(
-    (data) => {console.log(data)}
-);
+    (response) => {
+        return response.json()
+    })
+    .then((data) => {
+        console.log(data);
+    });
 
 httpService.makeRequest('http://api.openweathermap.org/data/2.5/find?lat=53.9&lon=27.5&cnt=50&lang=ru&APPID=103b41f82bea70d2198ab91ea029dcde').then(
-    (data) => {console.log(data)}
-);
+    (response) => {
+        return response.json()
+    })
+    .then((data) => {
+        console.log(data)
+    });
