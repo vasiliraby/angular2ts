70c6b65358b60c01d151494917e33944d1ec1f4b
Vasili Raby <vasiliraby@gmail.com>
2016-12-29 00:21:12 +0300

    Add weather refresh date component

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -12,6 +12,7 @@ import { AppComponent }        from './components/app/app.component';
 import { AppHeader }           from './components/header/header.component';
 import { AppFooter }           from './components/footer/footer.component';
 import { WeatherComponent }    from './components/weather/weather.component';
+import { WeatherRefreshDate }  from './components/weather-refresh-date/weather-refresh-date.component';
 import { MapComponent }        from './components/map/map.component';
 import { KelvinToCelciusPipe } from './pipes/kelvin-to-celcius.pipe';
 import { Preloader }           from './components/preloader/preloader.component';
@@ -30,6 +31,7 @@ const GOOGLEMAPS_API_KEY: string = 'AIzaSyAM7d7El7L7XpS5HWJYu2l8r-Yf7DjrGBQ';
         AppHeader,
         AppFooter,
         WeatherComponent,
+        WeatherRefreshDate,
         MapComponent,
         KelvinToCelciusPipe,
         Preloader
