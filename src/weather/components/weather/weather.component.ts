c9da781a9f8767c3590a369467e8b3d389aa2cd4
Vasili Raby <Vasili_Raby@epam.com>
2017-03-02 17:21:25 +0300

    Use angular http service

--- src/weather/components/weather/weather.component.ts
+++ src/weather/components/weather/weather.component.ts
@@ -1,8 +1,8 @@
 import { Component, OnInit, Input, ChangeDetectionStrategy, ChangeDetectorRef } from '@angular/core';
+import { Http, RequestMethod, URLSearchParams, Request, Response }              from '@angular/http';
 
 import { IWeatherItems }        from './weather-items.interface';
 import { RoundToPrecisionPipe } from './../../../app/pipes/round-to-precision.pipe';
-import { HttpService }          from './../../../app/services/http.service';
 
 @Component({
     selector: 'weather-component',
@@ -20,7 +20,7 @@ export class WeatherComponent implements OnInit {
     favoriteItem: Component;
     errorMsg: string;
 
-    constructor(private cd: ChangeDetectorRef) {
+    constructor(private cd: ChangeDetectorRef, private http: Http) {
         // Set weather update every 5 min
         setInterval(() => {
             this.getWeatherData(this.lat, this.lng);
@@ -36,25 +36,34 @@ export class WeatherComponent implements OnInit {
 
     private getWeatherData(lat: number, lng: number): void {
         const WEATHER_API_KEY: string = '103b41f82bea70d2198ab91ea029dcde';
-        let httpService: HttpService = new HttpService();
-        let url: string = 'http://api.openweathermap.org/data/2.5/find?lat=' + lat + '&lon=' + lng + '&cnt=30&lang=en&APPID=' + WEATHER_API_KEY;
+        const url: string = 'http://api.openweathermap.org/data/2.5/find';
 
-        httpService.makeRequest(url).then((data) => {
-            let weatherData = data.list;
-            let currentWeatherDate = Number(weatherData[0].dt + '000');
+        const urlParams = new URLSearchParams();
+        urlParams.append('lat', lat.toString());
+        urlParams.append('lon', lng.toString());
+        urlParams.append('cnt', '50');
+        urlParams.append('lang', 'en');
+        urlParams.append('APPID', WEATHER_API_KEY);
 
-            // Update the view only if the data was changed
-            if (this.actualWeatherDate !== currentWeatherDate) {
-                this.weatherItems = weatherData;
-                this.actualWeatherDate = currentWeatherDate;
-                this.weatherIsLoaded = true;
-                this.cd.markForCheck();
-            }
-        }, (reason: string) => {
-            this.weatherIsLoaded = false;
-            this.errorMsg = reason;
-            this.cd.markForCheck();
+        const request = new Request({
+            method: RequestMethod.Get,
+            url: url,
+            search: urlParams
         });
+
+        const response = this.http.request(request)
+            .subscribe((response: Response) => {
+                let weatherData = response.json().list;
+                let currentWeatherDate: number = Number(weatherData[0].dt + '000');
+
+                // Update the view only if the data was changed
+                if (this.actualWeatherDate !== currentWeatherDate) {
+                    this.weatherItems = weatherData;
+                    this.actualWeatherDate = currentWeatherDate;
+                    this.weatherIsLoaded = true;
+                    this.cd.markForCheck();
+                }
+            });
     }
 
     toggleFavorite(favItem: Component): void {
