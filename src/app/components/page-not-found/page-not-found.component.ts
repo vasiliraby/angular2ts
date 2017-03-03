e32b3b940339ca93d83dccec89593755fabc0fbb
Vasili Raby <vasiliraby@gmail.com>
2017-03-04 00:52:56 +0300

    Add some routing

---
+++ src/app/components/page-not-found/page-not-found.component.ts
@@ -0,0 +1,10 @@
+import { Component, ChangeDetectionStrategy } from '@angular/core';
+
+@Component({
+    selector: 'page-not-found',
+    templateUrl: 'src/app/components/page-not-found/page-not-found.component.html',
+    styleUrls: ['src/app/components/page-not-found/page-not-found.component.css'],
+    changeDetection: ChangeDetectionStrategy.OnPush
+})
+
+export class PageNotFoundComponent { }
